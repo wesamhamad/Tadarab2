c2c226327f761ce81c45040745f8a7ab512b0357
Wesam <74800962+wesamhamad@users.noreply.github.com>
2023-07-18 02:33:16 +0300

    Update Hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -13,11 +13,10 @@ const Hero = ({ heading, message }) => {
   };
 
   return (
-    // <div className='flex flex-col items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img text-right'>
-    <div className='flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img'>
+   <div className='flex flex-col items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img text-right'>
       {/* Overlay */}
       <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/50 z-[2]' />
-      <div className='p-5 text-white z-[2] mt-[3rem]'>
+      <div className='p-5 text-white z-[2] mt-[-10rem]'>
         <h2 className='text-5xl font-bold'>{heading}</h2>
         <p className='py-5 text-xl'>{message}</p>
         <button class="group relative h-12 w-48 overflow-hidden rounded-lg bg-white text-lg shadow" onClick={scrollToSection}>
