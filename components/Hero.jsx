0247710273551b9ce9c777ddf84a1a2d7fefc273
Wesam <74800962+wesamhamad@users.noreply.github.com>
2023-07-18 01:12:38 +0300

    Update Hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -13,19 +13,21 @@ const Hero = ({ heading, message }) => {
   };
 
   return (
-    <div className='flex flex-col items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img text-right'>
-      {/* Overlay */}
-      <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/50 z-[2]' />
-      <div className='p-5 text-white z-[2] mt-[3rem]'>
-        <h2 className='text-5xl font-bold'>{heading}</h2>
-        <p className='py-5 text-xl'>{message}</p>
-        <button class="group relative h-12 w-48 overflow-hidden rounded-lg bg-white text-lg shadow" onClick={scrollToSection}>
-          <div class="absolute inset-0 w-3 bg-gray-400 bg-opacity-50 transition-all duration-[250ms] ease-out group-hover:w-full"></div>
-          <span class="relative text-black group-hover:text-white">الـمزيد</span>
-        </button>
+    <div class="custom-img-container">
+      <div class="content">
+        {/* Overlay */}
+        <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/50 z-[2]' />
+        <div className='p-5 text-white z-[2] mt-[3rem]'>
+          <h2 className='text-5xl font-bold'>{heading}</h2>
+          <p className='py-5 text-xl'>{message}</p>
+          <button class="group relative h-12 w-48 overflow-hidden rounded-lg bg-white text-lg shadow" onClick={scrollToSection}>
+            <div class="absolute inset-0 w-3 bg-gray-400 bg-opacity-50 transition-all duration-[250ms] ease-out group-hover:w-full"></div>
+            <span class="relative text-black group-hover:text-white">الـمزيد</span>
+          </button>
 
+        </div>
+        <div ref={sectionRef} />
       </div>
-      <div ref={sectionRef} />
     </div>
   );
 };
